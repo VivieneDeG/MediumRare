fe4df6f346ccef333f6fa38472d652c89130117d
Viviene De Guzman <vivienedeg@gmail.com>
2019-02-26 16:45:30 -0800

    Initial commit, setup project

---
+++ frontend/medium_rare.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1><marquee>Welcome to MediumRare~~ :D</marquee></h1>, root);
+});
