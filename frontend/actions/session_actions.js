9f6e586097d75644e08189f2b6ac0b5a26be2bb9
Viviene De Guzman <vivienedeg@gmail.com>
2019-02-27 14:23:39 -0800

    Add session API util, session actions, and session reducer

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,33 @@
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors,
+});
+
+
+
+export const login = user => dispatch => (
+  SessionApiUtil.login(user).then( user => dispatch(receiveCurrentUser(user)) )
+);
+  
+export const logout = () => dispatch => (
+  SessionApiUtil.logout().then( () => dispatch(logoutCurrentUser()) )
+);
+    
+export const signup = user => dispatch => (
+  SessionApiUtil.signup(user).then( user => dispatch(receiveCurrentUser(user)) )
+);
