cccd1ec587969b1e4862dd0d269137ae2d70e81f
Viviene De Guzman <vivienedeg@gmail.com>
2019-02-27 18:48:18 -0800

    Add App and Root components, modify entry point

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1><marquee>MediumRare</marquee></h1>
+  </div>
+);
+
+export default App;
--- frontend/medium_rare.jsx
+++ frontend/medium_rare.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
@@ -10,5 +11,5 @@ document.addEventListener("DOMContentLoaded", () => {
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   
-  ReactDOM.render(<h1><marquee>Welcome to ~~MediumRare~~ :D</marquee></h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
