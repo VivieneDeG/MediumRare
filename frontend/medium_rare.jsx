3d83df07fda10511a747889da42c3132a8ea7660
Viviene De Guzman <vivienedeg@gmail.com>
2019-02-27 18:08:17 -0800

    Install redux-logger and redux-thunk libraries, add root reducer and store

--- frontend/medium_rare.jsx
+++ frontend/medium_rare.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1><marquee>Welcome to MediumRare~~ :D</marquee></h1>, root);
+  const store = configureStore();
+
+  // FOR TESTING
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
+  ReactDOM.render(<h1><marquee>Welcome to ~~MediumRare~~ :D</marquee></h1>, root);
 });
