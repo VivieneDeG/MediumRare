a9d15b299d30e74d12446d905a01dea63e31931c
Viviene De Guzman <vivienedeg@gmail.com>
2019-02-28 14:46:23 -0800

    Add seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ApplicationRecord.transaction do
+
+  User.destroy_all
+
+  # Users
+  demoUser = User.create!(name: "Guest User", email: "guest@demo.com", password: "watpassword")
+  user1 = User.create!(name: "Lissa Redbird", email: "spirit@element.org", password: "vampireacademy")
+  user2 = User.create!(name: "Zoey Aether", email: "night@nyx.io", password: "houseofnight")
+  user3 = User.create!(name: "Nicolas Flamel", email: "philosopher@alchemy.io", password: "harrypotter")
+  user4 = User.create!(name: "Raven Dragomir", email: "nevermore@trigon.com", password: "edgarapoe")
+  user5 = User.create!(name: "Viviene De G", email: "creator@mediumrare.com", password: "mediumrare")
+  user6 = User.create!(name: "Lancelot du Lac", email: "knight@roundtable.org", password: "kingarthur")
+end
+ 
