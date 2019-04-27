92b3b362e8e57a5584144e76a59b7563b585e019
Viviene De Guzman <vivienedeg@gmail.com>
2019-04-27 12:16:07 -0700

    Remove getState and dispatch from window

--- frontend/medium_rare.jsx
+++ frontend/medium_rare.jsx
@@ -19,10 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
-
-  // FOR TESTING
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
   
   ReactDOM.render(<Root store={store} />, root);
 });
